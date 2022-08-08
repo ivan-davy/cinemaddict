8e7e4febf9d4c90e856924684f7b0eff48b1a155
Ivan Davydov <imdavydov@outlook.com>
2022-08-09 02:44:35 +0300

    Реализация view-компонентов

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import NewShowMoreButtonView from './view/new-show-more-button-view';
 import NewPopupView from './view/new-popup-view';
 import NewFilmDatabaseStatsView from './view/new-film-database-stats-view';
 import NewFilmListContainerView from './view/new-film-list-container-view';
+import NewTopRatedContainerView from './view/new-top-rated-container-view';
+import NewMostCommentedContainerView from './view/new-most-commented-container-view';
 
 
 const siteHeaderElement = document.querySelector('.header');
@@ -23,6 +25,18 @@ for (let i = 0; i < 5; i++) {
   render(new NewFilmCardView(), filmListContainerElement);
 }
 render(new NewShowMoreButtonView(), siteMainElement.querySelector('.films-list'));
+
+const filmsElement = siteMainElement.querySelector('.films');
+render(new NewTopRatedContainerView(), filmsElement);
+const topRatedElement = filmsElement.querySelector('#top-rated');
+for (let i = 0; i < 2; i++) {
+  render(new NewFilmCardView(), topRatedElement.querySelector('.films-list__container'));
+}
+render(new NewMostCommentedContainerView(), filmsElement);
+const mostCommentedElement = filmsElement.querySelector('#most-commented');
+for (let i = 0; i < 2; i++) {
+  render(new NewFilmCardView(), mostCommentedElement.querySelector('.films-list__container'));
+}
 
 //render(new NewPopupView(), siteMainElement);
 render(new NewFilmDatabaseStatsView(), siteFooterElement.querySelector('.footer__statistics'));
