ca6d6cbf9f5a9488801193945ad6bde05da0e88e
Ivan Davydov <imdavydov@outlook.com>
2022-08-09 22:11:58 +0300

    Разделяет view для popup на 3 части (контейнер, верхняя часть, нижняя часть), вводит новый presenter для popup, исправляет недочеты.

--- src/utility/date-time-format.js
+++ src/utility/date-time-format.js
@@ -2,5 +2,6 @@ import dayjs from 'dayjs';
 
 const getPrettyYear = (year) => dayjs(year).format('YYYY');
 const getPrettyDate = (date) => dayjs(date).format('D MMMM YYYY');
+const getCommentDate = (date) => dayjs(date).format('YYYY/MM/DD HH:mm');
 
-export {getPrettyYear, getPrettyDate};
+export {getPrettyYear, getPrettyDate, getCommentDate};
