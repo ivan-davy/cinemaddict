a74011fdc1c1ad5e7163e6b0ee29484000c96df2
Ivan Davydov <imdavydov@outlook.com>
2022-08-11 15:23:46 +0300

    WIP

--- src/view/top-rated-container-view.js
+++ src/view/top-rated-container-view.js
@@ -1,11 +1,10 @@
 import {createElement} from '../render.js';
 
-const createTopRatedContainerTemplate = () => '' +
-  '<section class="films-list films-list--extra" id="top-rated">\n' +
-  '  <h2 class="films-list__title">Top Rated</h2>\n' +
-  '\n' +
-  '  <div class="films-list__container"></div>' +
-  '</section>';
+const createTopRatedContainerTemplate = () => `<section class="films-list films-list--extra" id="top-rated">
+    <h2 class="films-list__title">Top Rated</h2>
+
+    <div class="films-list__container"></div>
+  </section>`;
 
 export default class TopRatedContainerView {
   #element = null;
