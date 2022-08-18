1b4c7ba30e96fce5a54892463c156d53b6a4bb05
Ivan Davydov <imdavydov@outlook.com>
2022-08-19 02:50:59 +0300

    WIP :(

--- src/presenter/movie-presenter.js
+++ src/presenter/movie-presenter.js
@@ -1,23 +1,21 @@
 import MovieCardView from '../view/movie-card-view';
-import PopupPresenter from './popup-presenter';
 import {remove, render, replace} from '../framework/render';
 
 export default class MoviePresenter {
   #movieContainerElement = null;
   #movie = null;
   #comments = null;
-  #updateCards = null;
+  #updateData = null;
 
   #movieCardComponent = null;
   #popupPresenter = null;
 
-  constructor(movieContainerElement, mainElement, comments, updateCards) {
-    this.mainElement = mainElement;
+  constructor(movieContainerElement, comments, popupPresenter, updateData) {
     this.#movieContainerElement = movieContainerElement;
     this.#comments = comments;
-    this.#updateCards = updateCards;
+    this.#updateData = updateData;
 
-    this.#popupPresenter = new PopupPresenter(this.mainElement, this.#comments, this.#updateCards);
+    this.#popupPresenter = popupPresenter;
   }
 
   init(movie) {
@@ -39,8 +37,6 @@ export default class MoviePresenter {
     if (this.#movieContainerElement.contains(prevMovieCardComponent.element)) {
       replace(this.#movieCardComponent, prevMovieCardComponent);
     }
-
-    remove(prevMovieCardComponent);
   }
 
   destroy = () => {
@@ -49,22 +45,25 @@ export default class MoviePresenter {
 
 
   #movieClickHandler = () => {
-    this.#popupPresenter.init(this.#movie);
+    this.#popupPresenter.init();
+    this.#popupPresenter.popupShown = true;
   };
 
   #watchlistClickHandler = () => {
     this.#movie.userDetails.watchlist = !this.#movie.userDetails.watchlist;
-    this.#updateCards(this.#movie);
-    this.#popupPresenter.renderPopup();
+    this.#updateData(this.#movie);
+    this.#popupPresenter.init();
   };
 
   #historyClickHandler = () => {
     this.#movie.userDetails.alreadyWatched = !this.#movie.userDetails.alreadyWatched;
-    this.#updateCards(this.#movie);
+    this.#updateData(this.#movie);
+    this.#popupPresenter.init();
   };
 
   #favoriteClickHandler = () => {
     this.#movie.userDetails.favorite = !this.#movie.userDetails.favorite;
-    this.#updateCards(this.#movie);
+    this.#updateData(this.#movie);
+    this.#popupPresenter.init();
   };
 }
