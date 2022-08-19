fa76da4156d320ed1765c8d45750d5c39c44882e
Ivan Davydov <imdavydov@outlook.com>
2022-08-20 01:35:53 +0300

    Исправляет поведение попапа. Реализует сортировку.
    WIP Редкий баг - генерация лишнего фильма при смене сортировки

--- src/presenter/movie-presenter.js
+++ src/presenter/movie-presenter.js
@@ -46,7 +46,6 @@ export default class MoviePresenter {
 
   #movieClickHandler = () => {
     this.#popupPresenter.init();
-    this.#popupPresenter.popupShown = true;
   };
 
   #watchlistClickHandler = () => {
---
+++ src/utility/sort-logic.js
@@ -0,0 +1,42 @@
+import dayjs from 'dayjs';
+
+const SORT_TYPES = {
+  DEFAULT: 'default',
+  DATE_DOWN: 'date-down',
+  RATING_DOWN: 'rating-down',
+};
+
+const getWeightNullDate = (dateA, dateB) => {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
+  if (dateA === null) {
+    return 1;
+  }
+  if (dateB === null) {
+    return -1;
+  }
+  return null;
+};
+
+const sortDateDown = (movieA, movieB) => {
+  const weight = getWeightNullDate(movieA.filmInfo.release.date, movieB.filmInfo.release.date);
+  return weight ?? dayjs(movieB.filmInfo.release.date).diff(dayjs(movieA.filmInfo.release.date));
+};
+
+const sortRatingDown = (movieA, movieB) => {
+  if (movieA.filmInfo.totalRating > movieB.filmInfo.totalRating) {
+    return -1;
+  }
+  if (movieA.filmInfo.totalRating < movieB.filmInfo.totalRating) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+};
+
+
+export {SORT_TYPES, sortDateDown, sortRatingDown};
+
+
