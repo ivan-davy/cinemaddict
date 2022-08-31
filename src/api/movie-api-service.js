88b08dbe53e79bde3b5ad92f84b589ef2426340d
Ivan Davydov <imdavydov@outlook.com>
2022-08-31 18:28:57 +0300

    Добавляет адаптеры для данных фильмов

--- src/api/movie-api-service.js
+++ src/api/movie-api-service.js
@@ -23,4 +23,37 @@ export default class MovieApiService extends ApiService {
 
     return parsedResponse;
   };
+
+  #adaptToServer = (movie) => { //?
+    const adaptedMovie = {
+      ...movie,
+      'film_info': {
+        ...movie.filmInfo,
+        'alternative_title': movie.filmInfo.alternativeTitle,
+        'total_rating': movie.filmInfo.totalRating,
+        'age_rating': movie.filmInfo.ageRating,
+        'release': {
+          ...movie.filmInfo.release,
+          'release_country': movie.filmInfo.release.releaseCountry,
+        }
+      },
+      'user_details': {
+        ...movie.userDetails,
+        'already_watched': movie.userDetails.alreadyWatched,
+        'watching_date': movie.userDetails.watchingDate
+      }
+    };
+
+    delete adaptedMovie['filmInfo'];
+    delete adaptedMovie['userDetails'];
+    delete adaptedMovie['film_info']['alternativeTitle'];
+    delete adaptedMovie['film_info']['totalRating'];
+    delete adaptedMovie['film_info']['ageRating'];
+    delete adaptedMovie['film_info']['release']['releaseCountry'];
+    delete adaptedMovie['user_details']['alreadyWatched'];
+    delete adaptedMovie['user_details']['watchingDate'];
+
+    return adaptedMovie;
+  };
 }
+
--- src/model/movies-model.js
+++ src/model/movies-model.js
@@ -10,7 +10,7 @@ export default class MoviesModel extends Observable {
     super();
     this.#moviesApiService = moviesApiService;
     this.#moviesApiService.movies.then((movies) => {
-      console.log(movies);
+      console.log(movies.map(this.#adaptToClient));
     });
   }
 
@@ -59,5 +59,33 @@ export default class MoviesModel extends Observable {
     ];
 
     this._notify(updateType, update);
+  };
+
+  #adaptToClient = (movie) => {
+    const adaptedMovie = {...movie,
+      filmInfo: {...movie['film_info'],
+        alternativeTitle: movie['film_info']['alternative_title'],
+        totalRating: movie['film_info']['total_rating'],
+        ageRating: movie['film_info']['age_rating'],
+        release: {...movie['film_info']['release'],
+          releaseCountry: movie['film_info']['release']['release_country']
+        }
+      },
+      userDetails: {...movie['user_details'],
+        alreadyWatched: movie['user_details']['already_watched'],
+        watchingDate: movie['user_details']['watching_date']
+      }
+    };
+
+    delete adaptedMovie['film_info'];
+    delete adaptedMovie['user_details'];
+    delete adaptedMovie['filmInfo']['alternative_title'];
+    delete adaptedMovie['filmInfo']['total_rating'];
+    delete adaptedMovie['filmInfo']['age_rating'];
+    delete adaptedMovie['filmInfo']['release']['release_country'];
+    delete adaptedMovie['userDetails']['already_watched'];
+    delete adaptedMovie['userDetails']['watching_date'];
+
+    return adaptedMovie;
   };
 }
